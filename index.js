31cfbaa77e28bd3e0bb567e44e5215391f601313
Rohitash Kator <rohitashkator@Rohitashs-MacBook-Pro.local>
2022-05-10 09:34:33 +0530

    updated data

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const nodemailer = require('nodemailer');
 
-const PORT = process.env.PORT || 8200;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.static('public'));
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/contactfrom.html');
+app.use(cors());
 
+app.get('/', cors(), (req, res) => {
+    // res.send(`<p>Server is Running.</p>`)
+    res.sendFile(__dirname + '/public/templates/index.html');
 })
-app.post('/', (req, res) => {
+app.post('/send-mail', cors(), (req, res) => {
     console.log("Req.body", req.body);
 
     const transport = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: "YourEmail",
-            pass: "Your_Accounr_Password"
+            user: "sender-email@gmail.com",
+            pass: "password-sender-email"
         }
     });
 
     const mailOptions = {
         from: req.body.email,
-        to: 'Email address where you receive the query as mail',
-        subject: `Message Received from ${req.body.email}: ${req.body.subject}`,
+        to: 'receiver-mail',
+        subject: `New message Received from ${req.body.name}(${req.body.email}): ${req.body.subject}`,
         text: req.body.message
     }
 
@@ -42,5 +45,5 @@ app.post('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ', PORT);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 })
